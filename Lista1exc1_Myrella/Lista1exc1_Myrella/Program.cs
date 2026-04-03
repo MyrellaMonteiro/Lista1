1242eb16d7fb92e90cb9ebdb32275ed4a1fc3e64
MyrellaMonteiro <Myh.monteiro123@gmail.com>
2026-04-02 21:51:14 -0300

    Add files via upload

---
+++ Lista1exc1_Myrella/Lista1exc1_Myrella/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lista1exc1_Myrella
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int l;
+            int a;
+
+            Console.WriteLine("Informe a aresta do quadrado");
+            l = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("A area do quadrado é ");
+            a = l * l;
+            Console.WriteLine(a);
+        }
+    }
+}
---
+++ Lista1exc2_Myrella/Lista1exc2_Myrella/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lista1exc2_Myrella
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int b;
+            int h;
+            int a;
+
+            Console.WriteLine("Informe a base do reângulo:");
+            b = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("informe a altura do retângulo:");
+            h = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("A area do retângulo é ");
+            a = h * b;
+
+            Console.WriteLine(a);
+        }
+    }
+}
