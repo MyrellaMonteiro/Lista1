02077868b31239586b7f0dda2e396e3685e8bfc3
MyrellaMonteiro <Myh.monteiro123@gmail.com>
2026-04-02 22:36:06 -0300

    Add files via upload

---
+++ Lista1exc4_Myrella/Lista1exc4_Myrella/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lista1exc4_Myrella
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int b;
+            int h;
+            int a;
+
+            Console.WriteLine("Informe a base do triângulo:");
+            b = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Informe a altura do triângulo:");
+            h = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("A area do triângulo é ");
+            a = (b * h) / 2;
+            Console.WriteLine(a);
+        }
+    }
+}
---
+++ Lista1exc5_Myrella/Lista1exc5_Myrella/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lista1exc5_Myrella
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a;
+            int b;
+            int c;
+            int d;
+            int M;
+
+            Console.WriteLine("Insira o primeiro valor:");
+            a = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Insira o segundo valor");
+            b = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Insira o terceiro valor");
+            c = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Insira o Quarto valor");
+            d = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("A média aritimética dos valores apresentados é");
+            M = (a + b + c + d)/4;
+
+            Console.WriteLine(M);
+        }
+    }
+}
