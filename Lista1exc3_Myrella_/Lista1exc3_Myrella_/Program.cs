3d91f07202d1b90b7a0cdedd25e9c394c0e9dabc
MyrellaMonteiro <Myh.monteiro123@gmail.com>
2026-04-02 22:14:09 -0300

    Add files via upload

---
+++ Lista1exc3_Myrella_/Lista1exc3_Myrella_/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lista1exc3_Myrella_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int d;
+            int a;
+
+            Console.WriteLine("Informe a diagonal do quadrado:");
+            d = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("A area do quadrado é");
+            a = (d * d) / 2;
+
+            Console.Write(a);
+        }
+    }
+}
